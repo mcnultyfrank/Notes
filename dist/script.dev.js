0a5177c2e07fb98a642422afe1171fac06b2453a
Frank McNulty <frank.mcnulty@nology.io>
2020-10-09 13:22:30 +0100

    major responsive changes

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,30 +1,30 @@
 "use strict";
 
-var userInput = document.querySelector(".form-input"); // add new notes
+function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
+
+var userInput = document.querySelector(".user-input"); // add new notes
 
 var addNote = document.getElementById("save-button"); // submit new note
 
+var userText = document.querySelector("#user-text");
 var addToNoteSection = document.querySelector(".new-note-container"); // adds new note to note section
 
 var dummyText = ["hi there", "person"];
-var deleteButton = document.getElementById("delete-button");
+var deleteButton = document.querySelector("#delete-button");
 var yellow = document.querySelector(".yellow");
 var black = document.querySelector(".black");
 var red = document.querySelector(".red");
 var green = document.querySelector(".green");
-var newFolder = addToNoteSection.innerHTML = "";
-addNote.addEventListener("click", function (e) {
-  dummyText.forEach(function (event, index) {
-    if (index === 0) {
-      addToNoteSection.innerHTML += "<h3>".concat(event, "</h3>");
-    }
-  });
-});
-deleteButton.addEventListener("click", function (e) {
-  userInput.reset();
-});
-addNote.addEventListener("click", function (e) {
-  userInput.submit();
+var name = 'frank';
+var largeFontSize = +"30";
+var xLargeFontSize = largeFontSize + 5;
+var xxLargeFontSize = xLargeFontSize + 5;
+var xxxLargeFontSize = xxLargeFontSize + 5;
+var xxxxLargeFontSize = xxxLargeFontSize + 5;
+document.querySelector(".fa-plus").addEventListener("click", function (e) {
+  if (userText.style.fontSize = "25px") userText.style.fontSize = largeFontSize + "px";else if (userText.style.fontSize = "30px") {
+    userText.style.fontSize = xLargeFontSize + "px";
+  }
 });
 yellow.addEventListener("click", function (e) {
   document.querySelector("#user-text").style.color = "#F2E85C";
@@ -38,3 +38,23 @@ black.addEventListener("click", function (e) {
 red.addEventListener("click", function (e) {
   document.querySelector("#user-text").style.color = "#E50000";
 });
+deleteButton.addEventListener("click", function (e) {
+  document.querySelector(".form-input").reset();
+});
+addNote.addEventListener("click", function (e) {
+  var noteValue = "<h3>".concat(userInput.value, "<i class=\"fas remove-note fa-minus\"></i></h3>");
+
+  if (userInput.value !== "") {
+    addToNoteSection.innerHTML += noteValue;
+    document.querySelector(".form-input").reset();
+  }
+});
+addToNoteSection.addEventListener("click", function (e) {
+  console.log(userText);
+});
+document.querySelector(".remove-note").addEventListener("click", function (e) {
+  addToNoteSection += (_readOnlyError("addToNoteSection"), "");
+});
+document.querySelector("#user-text").style.alignContent = "flex-end"; // addNote.addEventListener("click", e => {
+//     userInput.submit();
+// })
--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-const userInput = document.querySelector(".form-input"); // add new notes
+const userInput = document.querySelector(".user-input"); // add new notes
 const addNote = document.getElementById("save-button"); // submit new note
+const userText = document.querySelector("#user-text");
 const addToNoteSection = document.querySelector(".new-note-container"); // adds new note to note section
 const dummyText = ["hi there", "person"];
-const deleteButton = document.getElementById("delete-button");
+const deleteButton = document.querySelector("#delete-button");
 const yellow = document.querySelector(".yellow");
 const black = document.querySelector(".black");
 const red = document.querySelector(".red");
 const green = document.querySelector(".green");
+const name = 'frank';
+const largeFontSize = +"30";
+const xLargeFontSize = largeFontSize + 5;
+const xxLargeFontSize = xLargeFontSize + 5;
+const xxxLargeFontSize = xxLargeFontSize + 5;
+const xxxxLargeFontSize = xxxLargeFontSize + 5;
 
-const newFolder = 
-addToNoteSection.innerHTML = "";
 
-addNote.addEventListener("click", e => {
-    dummyText.forEach((event, index) => {
-        if (index === 0){
-            addToNoteSection.innerHTML += `<h3>${event}</h3>`}
-    })
-})
-deleteButton.addEventListener("click", e => {
-    userInput.reset();
-})
-addNote.addEventListener("click", e => {
-    userInput.submit();
-    
-})
+
+
+document.querySelector(".fa-plus").addEventListener("click", e => {
+    if(userText.style.fontSize = "25px")
+    userText.style.fontSize = largeFontSize + "px";
+    else if (userText.style.fontSize = "30px"){
+        userText.style.fontSize = xLargeFontSize + "px";
+    }
+
+
+
+});
 yellow.addEventListener("click", e => {
     document.querySelector("#user-text").style.color = "#F2E85C";
     })
@@ -36,3 +40,30 @@ black.addEventListener("click", e => {
 red.addEventListener("click", e => {
                 document.querySelector("#user-text").style.color = "#E50000";
                 })
+
+
+deleteButton.addEventListener("click", e => {
+    document.querySelector(".form-input").reset();
+});
+addNote.addEventListener("click", e => {
+    const noteValue = `<h3>${userInput.value}<i class="fas remove-note fa-minus"></i></h3>`
+    if(userInput.value !== ""){
+    addToNoteSection.innerHTML += noteValue;
+    document.querySelector(".form-input").reset();
+    }
+});
+addToNoteSection.addEventListener("click", e => {
+    
+    console.log(userText);
+});
+document.querySelector(".remove-note").addEventListener("click", e =>{
+    addToNoteSection += "";
+});
+
+document.querySelector("#user-text").style.alignContent = "flex-end";
+
+
+// addNote.addEventListener("click", e => {
+//     userInput.submit();
+    
+// })
