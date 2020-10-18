4f782d821b843e446460401bd56861377d2b31a1
Frank McNulty <frank.mcnulty@nology.io>
2020-10-18 10:46:04 +0100

    major js edits

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,7 +1,5 @@
 "use strict";
 
-function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
-
 var userInput = document.querySelector(".user-input"); // add new notes
 
 var addNote = document.getElementById("save-button"); // submit new note
@@ -15,45 +13,73 @@ var yellow = document.querySelector(".yellow");
 var black = document.querySelector(".black");
 var red = document.querySelector(".red");
 var green = document.querySelector(".green");
+var bold = document.querySelector(".fa-bold");
+var italic = document.querySelector(".fa-italic");
+var alignLeft = document.querySelector(".fa-align-left");
+var alignRight = document.querySelector(".fa-align-right");
 var name = 'frank';
 var largeFontSize = +"30";
 var xLargeFontSize = largeFontSize + 5;
 var xxLargeFontSize = xLargeFontSize + 5;
 var xxxLargeFontSize = xxLargeFontSize + 5;
 var xxxxLargeFontSize = xxxLargeFontSize + 5;
+var fontSizes = [+"30", +"35", +"40", +"45", +"50"];
 document.querySelector(".fa-plus").addEventListener("click", function (e) {
-  if (userText.style.fontSize = "25px") userText.style.fontSize = largeFontSize + "px";else if (userText.style.fontSize = "30px") {
-    userText.style.fontSize = xLargeFontSize + "px";
+  for (var index = 0; index < fontSizes.length; index++) {
+    userText.style.fontSize = fontSizes[index] + "px";
+    console.log(fontSizes);
   }
-});
+}); // Colour change events
+
 yellow.addEventListener("click", function (e) {
-  document.querySelector("#user-text").style.color = "#F2E85C";
+  userText.style.color = "#F2E85C";
 });
 green.addEventListener("click", function (e) {
-  document.querySelector("#user-text").style.color = "#1E5601";
+  userText.style.color = "#1E5601";
 });
 black.addEventListener("click", function (e) {
-  document.querySelector("#user-text").style.color = "#0D0D0D";
+  userText.style.color = "#0D0D0D";
 });
 red.addEventListener("click", function (e) {
-  document.querySelector("#user-text").style.color = "#E50000";
+  userText.style.color = "#E50000";
+});
+bold.addEventListener("click", function (e) {
+  if (userText.style.fontWeight !== "600") {
+    userText.style.fontWeight = "600";
+  } else if (userText.style.fontWeight == "600") {
+    userText.style.fontWeight = "normal";
+  }
+
+  ;
+});
+italic.addEventListener("click", function (e) {
+  if (userText.style.fontStyle == "italic") {
+    userText.style.fontStyle = "normal";
+  } else if (userText.style.fontStyle !== "italic") {
+    userText.style.fontStyle = "italic";
+  }
+
+  ;
 });
 deleteButton.addEventListener("click", function (e) {
   document.querySelector(".form-input").reset();
 });
 addNote.addEventListener("click", function (e) {
-  var noteValue = "<h3>".concat(userInput.value, "<i class=\"fas remove-note fa-minus\"></i></h3>");
+  var noteValue = "<h2>".concat(userInput.value.substr(0, 20) + "...", "<i class=\"fas remove-note fa-minus\"></i></h2>");
 
   if (userInput.value !== "") {
     addToNoteSection.innerHTML += noteValue;
     document.querySelector(".form-input").reset();
   }
-});
-addToNoteSection.addEventListener("click", function (e) {
-  console.log(userText);
-});
+
+  addToNoteSection.addEventListener("click", function (e) {
+    userInput.value = addToNoteSection.innerText.slice(0, -3);
+  });
+}); //copy whatever text is in Notevalue and paste it in userInput
+
 document.querySelector(".remove-note").addEventListener("click", function (e) {
-  addToNoteSection += (_readOnlyError("addToNoteSection"), "");
+  console.log('hi');
+  addToNoteSection.innerText = "";
 });
 document.querySelector("#user-text").style.alignContent = "flex-end"; // addNote.addEventListener("click", e => {
 //     userInput.submit();
--- script.js
+++ script.js
@@ -8,56 +8,80 @@ const yellow = document.querySelector(".yellow");
 const black = document.querySelector(".black");
 const red = document.querySelector(".red");
 const green = document.querySelector(".green");
+const bold = document.querySelector(".fa-bold")
+const italic = document.querySelector(".fa-italic");
+const alignLeft = document.querySelector(".fa-align-left");
+const alignRight = document.querySelector(".fa-align-right");
 const name = 'frank';
 const largeFontSize = +"30";
 const xLargeFontSize = largeFontSize + 5;
 const xxLargeFontSize = xLargeFontSize + 5;
 const xxxLargeFontSize = xxLargeFontSize + 5;
 const xxxxLargeFontSize = xxxLargeFontSize + 5;
-
+const fontSizes = [+"30", +"35", +"40", +"45", +"50"];
 
 
 
 document.querySelector(".fa-plus").addEventListener("click", e => {
-    if(userText.style.fontSize = "25px")
-    userText.style.fontSize = largeFontSize + "px";
-    else if (userText.style.fontSize = "30px"){
-        userText.style.fontSize = xLargeFontSize + "px";
+  
+    for (let index = 0; index < fontSizes.length; index++) {
+        userText.style.fontSize = fontSizes[index] + "px";
+        console.log(fontSizes)
     }
 
 
-
 });
+// Colour change events
 yellow.addEventListener("click", e => {
-    document.querySelector("#user-text").style.color = "#F2E85C";
+    userText.style.color = "#F2E85C";
     })
 green.addEventListener("click", e => {
-        document.querySelector("#user-text").style.color = "#1E5601";
+    userText.style.color = "#1E5601";
         })
 black.addEventListener("click", e => {
-            document.querySelector("#user-text").style.color = "#0D0D0D";
+    userText.style.color = "#0D0D0D";
             })
 red.addEventListener("click", e => {
-                document.querySelector("#user-text").style.color = "#E50000";
+    userText.style.color = "#E50000";
                 })
+bold.addEventListener("click", e => {
+   
+    if (userText.style.fontWeight !== "600")
+    {userText.style.fontWeight = "600"}
+    else if(userText.style.fontWeight == "600"){
+        userText.style.fontWeight = "normal"}
+    ;
+})
+
+italic.addEventListener("click", e => {
+    if(userText.style.fontStyle == "italic"){
+        userText.style.fontStyle = "normal"
+    }else if (userText.style.fontStyle !== "italic")
+    {userText.style.fontStyle = "italic"}
+    ;
+})
+
 
 
 deleteButton.addEventListener("click", e => {
     document.querySelector(".form-input").reset();
 });
 addNote.addEventListener("click", e => {
-    const noteValue = `<h3>${userInput.value}<i class="fas remove-note fa-minus"></i></h3>`
+    const noteValue = `<h2>${userInput.value.substr(0, 20) + "..."}<i class="fas remove-note fa-minus"></i></h2>`
+    
     if(userInput.value !== ""){
-    addToNoteSection.innerHTML += noteValue;
+    addToNoteSection.innerHTML+= noteValue;
     document.querySelector(".form-input").reset();
     }
+    addToNoteSection.addEventListener("click", e => {
+        userInput.value = addToNoteSection.innerText.slice(0, -3);
+    });
 });
-addToNoteSection.addEventListener("click", e => {
-    
-    console.log(userText);
-});
+//copy whatever text is in Notevalue and paste it in userInput
+
 document.querySelector(".remove-note").addEventListener("click", e =>{
-    addToNoteSection += "";
+    console.log('hi');
+    addToNoteSection.innerText = "";
 });
 
 document.querySelector("#user-text").style.alignContent = "flex-end";
